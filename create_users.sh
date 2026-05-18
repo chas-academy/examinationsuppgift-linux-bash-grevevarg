355a39dd2c3958c5e520498fe9646949a53501c7
natsukashiixo <jesper.varg.viking@gmail.com>
2026-05-18 10:42:32 +0200

    uppdatera script

--- create_users.sh
+++ create_users.sh
@@ -1,59 +1,77 @@
 #!/bin/bash
 
-if ! [ $(id -u) == 0]; then
+if ! [ $(id -u) == 0 ]; then
   echo "script must run as root"
   exit 1
 fi
 
+cleanup() {
+    #yoinkar cleanup-funktionen från testet för bättre lokal testing :)
+    userlist=$@
+    for u in $userlist; do
+      userdel -r $u &>/dev/null
+      # hantera zombies
+      if [ ! -z "$u" ]; then rm -rf "/home/$u"; fi
+    done
+}
+
+cleanup $@
+
 # vet inte om grupp 1000 existerar i CD/CI där poängen räknas ut så fular mig lite
-sudo groupadd -f -g 1000
+sudo groupadd -f -g 1000 students
 
 USER_LIST=$@
 DOCDIR="Documents/"
 DOWNDIR="Downloads/"
 WORKDIR="Work/"
 
+# skapar användarens hem-mapp
 userdirs_create() {
   username=$1
   basepath="/home/$username"
-  # hade kunnat köra lista men nästa loop for 3 element känns fel
+  # hade kunnat köra lista men nästa in en loop for 3 element känns fel
   mkdir -p "$basepath/$DOCDIR"
   mkdir -p "$basepath/$DOWNDIR"
   mkdir -p "$basepath/$WORKDIR"
 }
 
+# skapar välkomstfilen för användaren
 welcome_create() {
   username=$1
   userhome="/home/$username/"
   welcomefile="$userhome/welcome.txt"
-  echo "Välkommen $username \n" > "$welcomefile"
+  echo "Välkommen $username /n" > "$welcomefile"
   # hoppas inte det är olagligt att googla lösningar
   # tyckte att det hade varit fult med alla UID/GID genom att bara köra
   # echo >> /etc/passwd
   # kanske borde läsa igenom testerna iofs, de kanske vill ha med all den datan
-  echo "$(cut -d: -f1 /etc/passwd')" >> "$welcomefile"
+  echo "$(cut -d: -f1 /etc/passwd)" >> "$welcomefile"
 }
 
+# fixar ägandeskap och sätter u:rwx g:--- o:---, båda är rekursiva
 userdirs_r_chown() {
   username=$1
   basepath="/home/$username"
   # för att se till att allting i användarens $HOME faktiskt ägs av dem samt defaultgruppen
   # insåg i efterhand att jag hade kunnat skapa mapparna med sudo --user $username tidigare
-  sudo chown -r "$username:1000" "$basepath"
-  sudo chmod -r 600 "$basepath"
+  sudo chown -R "$username:1000" "$basepath"
+  sudo chmod -R 700 "$basepath"
 }
 
+# skapa användare först innan övriga funktioner görs för att alla tests ska fungera
+# läggs till i grupp 1000 för att hellre safe than sorry
 for u in $USER_LIST; do
+  sudo useradd -m -g 1000 $u
+done
 
-  # lägg till användare, skapa home, sätt som grupp 1000 typ generisk Employee eller liknande
-  # vänta den kanske inte existerar i CD/CI pipelines? men useradd -D på mitt system printar GROUP=1000
-  # så det känns som att den borde existera? fast hellre safe than sorry
-  useradd -m -g 1000 $u
+for u in $USER_LIST; do
+  # separat loop för att skapa förväntad struktur + filer
 
   userdirs_create $u
+  # kul, detta testet failar för att jag skapar användarna i samma loop som jag skapar mappar/filer
+  # så kommer det inte vara när ni ser detta, men jag gillar att raljera i kommentarer
   welcome_create $u
   userdirs_r_chown $u
 
 done
-
 exit 0
